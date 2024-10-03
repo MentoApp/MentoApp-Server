d0e7ff9c6fa8104d4e01279c7c0b0940eac2521d
Sunro1994 <gwangjulsr@gmail.com>
2024-10-03 16:14:47 +0900

    User 조회 API추가, 한글 입력,반환 Desrializer 추가

--- src/main/java/com/mentit/mento/domain/users/repository/BoardKeywordRepository.java
+++ src/main/java/com/mentit/mento/domain/users/repository/BoardKeywordRepository.java
@@ -1,8 +1,9 @@
 package com.mentit.mento.domain.users.repository;
 
-import com.mentit.mento.domain.users.constant.BoardKeyword;
 import com.mentit.mento.domain.users.entity.BoardKeywordEntity;
+import com.mentit.mento.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardKeywordRepository extends JpaRepository<BoardKeywordEntity, Long> {
+    void deleteAllByUsers(Users findUserByUserDetail);
 }
