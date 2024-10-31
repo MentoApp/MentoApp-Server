0716254e3075c32e1494cf41aa1b358174d38877
Sunro1994 <gwangjulsr@gmail.com>
2024-10-31 19:27:16 +0900

    다이렉트 요청을 받을 임시 api생성

---
+++ src/main/java/com/mentit/mento/domain/users/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.mentit.mento.domain.users.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @GetMapping("/intermediate")
+    public ResponseEntity<Map<String, Object>> redirectToFrontend(
+            @RequestParam String accessToken,
+            @RequestParam boolean isNewUser) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("accessToken", accessToken);
+        response.put("isNewUser", isNewUser);
+
+        return ResponseEntity.ok(response);
+    }
+}
